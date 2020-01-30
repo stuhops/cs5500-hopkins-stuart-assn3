baedcc9cda4ffa7e6fbd00afe30aa36f1cb4575a
stuhops <stuart.hopkins@aggiemail.usu.edu>
2020-01-30 12:30:32 -0700

    Got the list distrobution working

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <mpi.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <time.h>
 
 using namespace std;
 
@@ -10,44 +11,61 @@ using namespace std;
 int main(int argc, char **argv){
 
     int rank, size;
-    int cnt = -1;
+    const int ARR_SIZE = 10; 
+    int data[ARR_SIZE];
     const int MAX_TIME = 10;
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MCW,&rank);
     MPI_Comm_size(MCW,&size);
+    MPI_Status mystatus;
 
-    // Use process 0 to initialize the count
-    if(!rank && cnt == -1) {
-      srand(5337228);
-      cnt = rand() % MAX_TIME; 
-      MPI_Send(&cnt, 1, MPI_INT, rand() % size, 0, MCW);
-    }
-
-    // While loop that receives, decrements, and passes the potato
-    while(true) {
-      MPI_Recv(&cnt, 1, MPI_INT, MPI_ANY_SOURCE, 0, MCW,MPI_STATUS_IGNORE);
-
-      if(!cnt) {
-        cout << "I am " << rank << " of " << size << "; got a countdown of " << cnt << " and I am the LOSER." << endl;
-        --cnt;
-        MPI_Send(&cnt, 1, MPI_INT, size + cnt, 0, MCW);  // Sends to the highest rank to begin termination
+    // Use process 0 as the master process
+    if(!rank) {
+      srand(time(NULL));
+      // Initialize the array
+      // Start at 1 because this process is already rank 0
+      for(int i = 1; i < ARR_SIZE; i++) {
+        data[i] = rand() % ARR_SIZE;
+        cout << " " << data[i];
       }
-      else if(cnt > 0) {
-        cout << "I am " << rank << " of " << size << "; got a countdown of " << cnt << endl;
-        sleep(1);
-        --cnt;
-        MPI_Send(&cnt, 1, MPI_INT, rand() % size, 0, MCW);
-      }
-      else {
-        // Decrement though all precesses to terminate while loop
-        --cnt;
-        int next_process = size + cnt;
-        if(next_process > -1) {
-          MPI_Send(&cnt, 1, MPI_INT, next_process, 0, MCW);
+      cout << "\n\n\n";
+
+      // Send out random chunks to processes
+      int size_left = ARR_SIZE;
+      int TMP_ARR_SIZE;
+      for(int i = 1; i < size; i++) {
+        if(!size_left) break;
+
+
+        if(i+1 != size) {
+          int prev_size_left = size_left;
+          size_left -= rand() % size_left;
+          TMP_ARR_SIZE= prev_size_left - size_left;
+          sleep(1);
+          MPI_Send(&data[size_left], TMP_ARR_SIZE, MPI_INT, i, 0, MCW);
         }
-        break;
+        else
+          MPI_Send(&data[1], size_left-1, MPI_INT, i, 0, MCW);
       }
+
     }
+
+    // Sort array in all processes except 0
+    else {
+      MPI_Probe(MPI_ANY_SOURCE, 0, MCW, &mystatus);
+      int count;
+      MPI_Get_count(&mystatus, MPI_INT, &count);
+      MPI_Recv(data, count, MPI_INT, MPI_ANY_SOURCE, 0, MCW, &mystatus);
+
+      cout << "Array received by rank " << rank << " ... Received a count of " << count << endl;
+      for(int i = 0; i < count; i++)
+        cout << " " << data[i];
+
+      cout << endl;
+    }
+
+    // MPI_Send(data, 1, MPI_INT, rand() % size, 0, MCW);
+    // MPI_Recv(data, 1, MPI_INT, MPI_ANY_SOURCE, 0, MCW, &mystatus);
 
     MPI_Finalize();
 }
